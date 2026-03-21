802082d137044a11383bb8fb9d40e82d0d7d0706
ElStron <luisstron45@gmail.com>
2026-03-20 21:29:34 -0600

    First commit

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,34 @@
+export const aboutMe = {
+  name: 'Soy Stron',
+  profession: 'Software Developer',
+  description: 'Passionate about building web applications and learning new technologies.',
+  bio: {
+    paragraphs: [
+      "I'm a passionate software developer with over 5 years of experience in building web applications. I specialize in JavaScript and have a strong background in both frontend and backend development.",
+      "I enjoy working on challenging projects and continuously learning new technologies. In my free time, I love traveling, cooking, and gaming.",
+    ]
+  },
+  skills: ['JavaScript', 'TypeScript', 'React', 'Node.js', 'Python'],
+  hobbies: ['Traveling', 'Cooking', 'Gaming'],
+  profile: {
+    picture: '/profile.jpg',
+    altText: "John Doe's Profile Picture",
+  },
+  banner: {
+    image: 'https://picsum.photos/id/4/750/200',
+    altText: 'Technology Banner',
+  },
+  contact: {
+    email: 'JohnDoe@example.com',
+    phone: '+1234567890',
+  }, 
+  socialMedia: {
+    linkedin: 'https://github.com/elstron',
+    github: 'https://github.com/elstron',
+    youtube: 'https://www.youtube.com/@Stron_',
+    x: 'https://x.com/Stron__',
+    website: 'https://github.com/elstron'
+    //facebook: 'https://www.facebook.com/JohnDoe',
+    //instagram: 'https://www.instagram.com/JohnDoe',
+  }
+};
---
+++ src/data/skills.ts
@@ -0,0 +1,41 @@
+import type { SvgComponent } from 'astro/types';
+import type { ImageMetadata } from 'astro';
+
+import Typescript from '@/assets/icons/typescript.svg';
+import React from '@/assets/icons/reactjs.svg';
+import Php from '@/assets/icons/php.svg';
+import Astro from '@/assets/icons/astro.svg';
+import Tailwind from '@/assets/icons/tailwind.svg';
+import Python from '@/assets/icons/python.svg';
+import Next from '@/assets/icons/next.svg';
+type Skill = {
+  name: string;
+  icon: SvgComponent & ImageMetadata 
+}
+
+export const skills = [
+  {
+    name: 'Typescript',
+    icon: Typescript,
+  },
+  {
+    name: 'React',
+    icon: React,
+  },{
+    name: 'Php',
+    icon: Php,
+  },{
+    name: 'Astro',
+    icon: Astro,
+  },{
+    name: 'Tailwind',
+    icon: Tailwind,
+  },{
+    name: 'Python',
+    icon: Python,
+  },
+  {
+    name: 'Next',
+    icon: Next,
+  },
+] as Skill[];
